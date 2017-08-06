600e31edd085a95e3baf1d014158ef4bb85ed636
Igor Onishchenko <optimum.flaminis@gmail.com>
2017-08-07 02:46:13 +0300

    init with django 1.11.3

---
+++ beerstars/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for beerstars project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beerstars.settings")
+
+application = get_wsgi_application()
